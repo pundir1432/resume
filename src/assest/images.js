083b5448c370f482bdc17acfb2f776cba9cb6b45
pawan1432 <p.pawan@rowthtech.in>
2024-11-05 12:21:28 +0530

    df

--- src/assest/images.js
+++ src/assest/images.js
@@ -1,2 +1,3 @@
 import about from './WhatsApp Image 2024-11-04 at 6.43.19 PM.jpeg'
-export {about}
+import logo from './WhatsApp Image 2024-11-05 at 12.00.37 PM.jpeg'
+export {about,logo}
--- src/componets/pwn portfolio/Section.js
+++ src/componets/pwn portfolio/Section.js
@@ -11,13 +11,13 @@ import * as yup from 'yup';
 import { FaTwitter, FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 import './Section.css'
 import Slider from 'react-slick';
-import { about } from '../../assest/images';
+import { about, logo } from '../../assest/images';
 const skillsData = [
-  { name: 'C',  backgroundImage: 'https://img.freepik.com/free-vector/branding-identity-corporate-c-logo-vector-design-template_460848-13936.jpg?semt=ais_hybrid' },
+  { name: 'C', backgroundImage: 'https://img.freepik.com/free-vector/branding-identity-corporate-c-logo-vector-design-template_460848-13936.jpg?semt=ais_hybrid' },
   { name: 'HTML5', backgroundImage: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_640.png' },
   { name: 'CSS3', backgroundImage: 'https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582747_1280.png' },
   { name: 'JavaScript', backgroundImage: 'https://www.shutterstock.com/image-illustration/javascript-es6-vector-logo-disign-260nw-2438402733.jpg' },
-  { name: 'SQL',  backgroundImage: 'https://www.shutterstock.com/image-vector/sql-icon-major-database-format-260nw-1904337712.jpg' },
+  { name: 'SQL', backgroundImage: 'https://www.shutterstock.com/image-vector/sql-icon-major-database-format-260nw-1904337712.jpg' },
   { name: 'ReactJS', backgroundImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlGmKtrnxElpqw3AExKXPWWBulcwjlvDJa1Q&s' },
   { name: 'ExpressJS', backgroundImage: 'https://t3.ftcdn.net/jpg/03/47/42/98/360_F_347429829_LhBd6fIJYzWZXg21HfDBm7Ze38akG9DS.jpg' },
 ];
@@ -93,6 +93,7 @@ function Hero() {
     autoplaySpeed: 2000,
     centerMode: true,
     centerPadding: "0px",
+    arrows: false,
     responsive: [
       {
         breakpoint: 1024,
@@ -115,22 +116,18 @@ function Hero() {
   return (
     <>
 
-      <div className={`container-fluid   ${scrolled ? 'scrolled' : ''}`}>
+      <div className={`container   ${scrolled ? 'scrolled' : ''}`}>
         <div className="row mx-auto">
           <div className="col-12 border-0">
             <nav className={`navbar navbar-expand-lg ${scrolled ? 'bg-change' : 'header-bg'} fixed-top border-none`}>
-              <div className="container-fluid">
-                <button
-                  className="navbar-toggler border-none"
-                  type="button"
-                  data-bs-toggle="collapse"
-                  data-bs-target="#navbarSupportedContent"
-                  aria-controls="navbarSupportedContent"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon"></span>
-                </button>
+            
+              <div className="container-fluid ">
+              <a href="" className="navbar-brand mb-3 p-0">
+                <img src={logo} alt="" className='img-fluid me-auto mt-2 rounded-circle d-lg-none' style={{width:'40px', height:'40px'}} />
+              </a>
+              <button className="navbar-toggler text-white border-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <i class="bi bi-list"></i>
+    </button>
                 <div className="collapse navbar-collapse p" id="navbarSupportedContent">
                   <ul className="navbar-nav ps-5  me-auto mb-2 mb-lg-0">
                     <li className="nav-item ps-5" onClick={() => scrollToSection('home')}>
@@ -169,23 +166,27 @@ function Hero() {
       <section id="home">
         <div className="container-fluid h-screen mt-5 ">
           <div className="row">
-            <div className="col-6 mt-5 ">
+            <div className="col-lg-6 col-12 mt-5 ">
               <div className="div text-white d-flex justify-content-start align-items-start">
                 <div className="div athira" data-aos="fade-down-right">
                   <h4>PANKAJ SINGH</h4>
                   <p>Engineer + Junior Front-end Developer</p>
-                  <div className="flex justify-center space-x-6 mt-5">
-                    <a href="https://twitter.com" target="_blank" className='rounded-circle py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                      <FaTwitter size={32} className='text-dark' />
+                  <div className="d-flex justify-content-center space-x-6 mt-5">
+                    <a href="https://twitter.com" target="_blank" className='rounded-circle  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaTwitter size={32} className='text-dark d-none d-lg-block' />
+                      <FaTwitter size={23} className='text-dark d-lg-none' />
                     </a>
-                    <a href="https://github.com" target="_blank" className='rounded-circle mx-2 py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                      <FaGithub size={32} className='text-dark' />
+                    <a href="https://github.com" target="_blank" className='rounded-circle mx-2  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaGithub size={32} className='text-dark d-none d-lg-block' />
+                      <FaGithub size={23} className='text-dark d-lg-none' />
                     </a>
-                    <a href="https://linkedin.com" target="_blank" className='rounded-circle mx-2 py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                      <FaLinkedin size={32} className='text-dark' />
+                    <a href="https://linkedin.com" target="_blank" className='rounded-circle mx-2  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaLinkedin size={32} className='text-dark d-none d-lg-block' />
+                      <FaLinkedin size={23} className='text-dark d-lg-none' />
                     </a>
-                    <a href="https://instagram.com" target="_blank" className='rounded-circle mx-2 py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                      <FaInstagram size={32} className='text-dark' />
+                    <a href="https://instagram.com" target="_blank" className='rounded-circle mx-2  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaInstagram size={32} className='text-dark d-none d-lg-block' />
+                      <FaInstagram size={23} className='text-dark d-lg-none' />
                     </a>
                   </div>
                 </div>
@@ -201,35 +202,42 @@ function Hero() {
       {/* ABOUT */}
       <section id="about">
         <div className="container mt-lg-5 ">
-          <div className="row">
-            <div className="col-12 col-lg-5 d-lg-flex flex-column justify-content-lg-end" data-aos="zoom-out-right">
-              <img src={about} className='img-fluid buissness-img ms-auto' alt="" />
+          <div className="row mx-auto">
+            {/* data-aos="zoom-out-right" */}
+            <div className="col-12 col-lg-5 d-lg-flex flex-column justify-content-lg-end" >
+              <img src={about} className='img-fluid buissness-img ms-auto mt-lg-0 mt-5' alt="" />
               <p style={{ color: 'black' }} className='mx-5 mt-4'><i class="bi bi-arrow-down-circle"></i> Scroll</p>
 
             </div>
-            <div className="col-12 col-lg-7 d-flex justify-content-center align-items-center  " data-aos="zoom-out-left">
+            {/* data-aos="zoom-out-left" */}
+            <div className="col-12 col-lg-7 d-flex justify-content-center align-items-center  " >
               <div className="div-about text-start  w-75">
                 <h3>About me</h3>
                 <p className="py-2 text-muted">
                   An unquisitive Computer Sceince Engineering student, skilled in leadership, seeking to leverage solid development skills with focus on collabration, communcation and passion.
                 </p>
-                <button className="btn  p-3 px-3 text-white" style={{ backgroundColor: '#151E3D' }}>Download CV</button>
+                <button className="btn  p-1  px-1 text-white d-lg-none" style={{ backgroundColor: '#151E3D', fontSize: '12px' }}>Download CV</button>
+
+                <button className="btn  p-3 px-3 text-white d-none d-lg-block" style={{ backgroundColor: '#151E3D' }}>Download CV</button>
               </div>
             </div>
           </div>
         </div>
+        <hr className='d-lg-none' />
       </section>
       {/* SKILLS */}
       <section id="skills">
-        <div className="container mt-5">
+        <div className="container mt-lg-5">
           <div className="row">
-            <div className="col-12 col-lg-7 d-flex justify-content-center align-items-center  " data-aos="zoom-out-right">
-              <div className="div-about text-start mt-5 w-75">
+            {/* data-aos="zoom-out-right" */}
+            <div className="col-12 col-lg-7 d-flex justify-content-center align-items-center  " >
+              <div className="div-about text-start mt-lg-5 w-75">
                 <h3>Skills</h3>
                 <p className="py-2 text-muted">
                   I enjoy creating things that live on the internet whether that be websites, applications, or anything in between.
                 </p>
-                <div className="skills-container mt-5" data-aos="fade-up" data-aos-anchor-placement="center-bottom">
+                {/* data-aos="fade-up" data-aos-anchor-placement="center-bottom" */}
+                <div className="skills-container mt-5" >
                   {skillsData.map((skill) => (
                     <div className="skill" key={skill.name}>
                       <div
@@ -249,7 +257,8 @@ function Hero() {
                 <p style={{ color: '#928A97' }} className='mt-5'><i class="bi bi-arrow-down-circle"></i> Scroll</p>
               </div>
             </div>
-            <div className="col-12 col-lg-5" data-aos="zoom-out-left">
+            {/* data-aos="zoom-out-left" */}
+            <div className="col-12 col-lg-5" >
               <img src="https://www.shutterstock.com/image-photo/young-casual-business-woman-sitting-600nw-2461145297.jpg" className='img-fluid buissness-img2 ms-auto' alt="" />
 
             </div>
@@ -258,13 +267,16 @@ function Hero() {
       </section>
       {/* WORKS */}
       <section id="works">
-        <div className="caontainer mt-4">
+
+        <div className="caontainer mt-lg-4">
           <div className="row mx-auto">
             <div className="col-12">
+              <hr className="d-lg-none" />
+
               <div className="carousel-container">
                 <Slider {...settings}>
                   {images.map((imgSrc, index) => (
-                    <div key={index} className="carousel-slide w-100 pt-5 pb-5 mt-4">
+                    <div key={index} className="carousel-slide w-100 pt-lg-5 pb-5 mt-lg-4">
                       <img src={imgSrc} alt={`Slide ${index + 1}`} className="carousel-image  mx-auto" />
                     </div>
                   ))}
@@ -282,15 +294,17 @@ function Hero() {
               <div className="col-12 bg-light p-lg-3">
                 <div className="row">
                   <div className="col-12 col-lg-5">
-                    <div className="div d-flex justify-content-center align-items-center" data-aos="flip-left"
-                      data-aos-easing="ease-out-cubic"
+                  {/* data-aos="flip-left"
+                      data-aos-easing="ease-out-cubic" */}
+                    <div className="div d-flex justify-content-center align-items-center" 
                       data-aos-duration="3000" >
-                      <h2 className='form-heading w-75 mt-lg-5'>Drop me a line I would like to hear from you.</h2>
+                      <h2 className='form-heading  mt-lg-5 mt-3' >Drop me a line I would like to hear from you.</h2>
                     </div>
                   </div>
                   <div className="col-12 col-lg-7"  >
                     <form onSubmit={handleSubmit(onSubmit)}>
-                      <h3 className='form-heading' data-aos="zoom-out">Get in Touch</h3>
+                    {/* data-aos="zoom-out" */}
+                      <h3 className='form-heading px-2 px-lg-0' >Get in Touch</h3>
 
                       {/* Name Input */}
                       <input
@@ -299,7 +313,7 @@ function Hero() {
                         className='form-control mb-3 input'
                         {...register('name')}
                       />
-                      {errors.name && <p className="text-danger">{errors.name.message}</p>}
+                      {errors.name && <p className="text-danger" style={{fontSize:'13px'}}>{errors.name.message}</p>}
 
                       {/* Email Input */}
                       <input
@@ -308,7 +322,7 @@ function Hero() {
                         className='form-control mb-3 input'
                         {...register('email')}
                       />
-                      {errors.email && <p className="text-danger">{errors.email.message}</p>}
+                      {errors.email && <p className="text-danger" style={{fontSize:'13px'}}>{errors.email.message}</p>}
 
                       {/* Message Textarea */}
                       <textarea
@@ -316,14 +330,21 @@ function Hero() {
                         className='form-control mb-3 input'
                         {...register('message')}
                       />
-                      {errors.message && <p className="text-danger">{errors.message.message}</p>}
+                      {errors.message && <p className="text-danger" style={{fontSize:'13px'}}>{errors.message.message}</p>}
 
                       {/* Submit Button */}
                       <button
                         type="submit"
-                        className="btn p-2 px-3 text-white"
+                        className="btn p-2 px-3 text-white d-none d-lg-block"
                         data-aos="zoom-out"
                         style={{ backgroundColor: '#151E3D' }}
+                      >
+                        Send Message
+                      </button>
+                      <button
+                        type="submit"
+                        className="btn p-1 px-1 text-white d-lg-none mb-2"
+                        style={{ backgroundColor: '#151E3D',fontSize:'13px' }}
                       >
                         Send Message
                       </button>
@@ -339,20 +360,24 @@ function Hero() {
             <div className="row mx-auto">
               <div className="col-12 text-center p-3 contact-section" data-aos="flip-down">
                 <p className=' text-white'>Pankaj singh</p>
-                <div className="flex justify-center space-x-6 ">
-                  <a href="https://twitter.com" target="_blank" className='rounded-circle py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                    <FaTwitter size={32} className='text-dark' />
-                  </a>
-                  <a href="https://github.com" target="_blank" className='rounded-circle mx-2 py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                    <FaGithub size={32} className='text-dark' />
-                  </a>
-                  <a href="https://linkedin.com" target="_blank" className='rounded-circle mx-2 py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                    <FaLinkedin size={32} className='text-dark' />
-                  </a>
-                  <a href="https://instagram.com" target="_blank" className='rounded-circle mx-2 py-3 p-0 px-2 icon' rel="noopener noreferrer">
-                    <FaInstagram size={32} className='text-dark' />
-                  </a>
-                </div>
+                <div className="d-flex justify-content-center space-x-6 mt-3">
+                    <a href="https://twitter.com" target="_blank" className='rounded-circle  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaTwitter size={32} className='text-dark d-none d-lg-block' />
+                      <FaTwitter size={23} className='text-dark d-lg-none' />
+                    </a>
+                    <a href="https://github.com" target="_blank" className='rounded-circle mx-2  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaGithub size={32} className='text-dark d-none d-lg-block' />
+                      <FaGithub size={23} className='text-dark d-lg-none' />
+                    </a>
+                    <a href="https://linkedin.com" target="_blank" className='rounded-circle mx-2  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaLinkedin size={32} className='text-dark d-none d-lg-block' />
+                      <FaLinkedin size={23} className='text-dark d-lg-none' />
+                    </a>
+                    <a href="https://instagram.com" target="_blank" className='rounded-circle mx-2  py-2 p-0 px-2 icon' rel="noopener noreferrer">
+                      <FaInstagram size={32} className='text-dark d-none d-lg-block' />
+                      <FaInstagram size={23} className='text-dark d-lg-none' />
+                    </a>
+                  </div>
                 <p className='mt-3 text-white'>All Rights Reserved &copy; {new Date().getFullYear()} Pankaj Singh</p>
               </div>
             </div>
